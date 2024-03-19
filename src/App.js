21ec39f496678e49b9bc8b98a4accac75979668e
deepeshkumar06 <deepeshkumar.p2004@gmail.com>
2024-03-19 10:44:43 +0530

    UPDATED

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import User from './components/User';
+import { Auth } from './components/Auth';
+import { Signup } from './components/Signup';
+import Admin from './components/Admin';
+import Feedbackcollect from './components/Feedbackcollect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/user' element={<User/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/admin' element={<Admin/>} />
+        <Route path='/feedback' element={<Feedbackcollect/>} />
+      </Routes>
+      </Auth>
     </div>
   );
 }
